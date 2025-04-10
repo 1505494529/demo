cbbb2305a2fc5dac0d179f8f277a7ac53bc1d076
ak dream <82067885+1505494529@users.noreply.github.com>
2025-04-10 16:18:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import { GoogleGenAI } from "@google/genai";
 import express from 'express';
 const ai = new GoogleGenAI({ apiKey: "AIzaSyDP7o1OSxj_9yrD1IIA4TM_ti-i32f9gqA" });
 const app = express();
+app.get('/', async (req, res) => {
+  res.send("hello");
+});
 app.get('/:prompt', async (req, res) => {
   const response = await ai.models.generateContent({
     model: "gemini-2.5-pro-exp-03-25",
